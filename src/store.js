d0053d51820986aa70dc54cce96de051a097c862
unknown <komaltiwary97@gmail.com>
2023-04-26 21:11:19 +0530

    store boilerplate

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createstore } from "redux";
+
+const store = () => {
+    return(
+        <div>
+            <h1>this is redux component</h1>
+        </div>
+    )
+}
+export dafault createstore();
